c3ee349e943395e2835803049a87ad192f4f572f
William Kent <wjk@users.noreply.github.com>
2019-09-19 19:57:50 -0400

    Remove remains of internal-only comment

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/ms/Internal/Markup/StringValueSerializer.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/ms/Internal/Markup/StringValueSerializer.cs
@@ -2,14 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
 //  Contents:  Stub value serializer for strings
-//
-//  Created:   04/28/2005 Microsoft
-//
 
 using System.Windows.Markup;
 
