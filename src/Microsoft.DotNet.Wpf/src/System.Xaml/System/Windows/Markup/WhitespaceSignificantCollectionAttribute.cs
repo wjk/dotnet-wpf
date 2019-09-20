38da832dd96d8dbc27057dddf229c58cc724d7a3
William Kent <wjk@users.noreply.github.com>
2019-09-19 20:17:13 -0400

    Remove remnant of Microsoft-internal comment

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/WhitespaceSignificantCollectionAttribute.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/WhitespaceSignificantCollectionAttribute.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
 //  Description: Specifies if a collection considers whitespacing to be significant for each of its "children."
 
 using System.Runtime.CompilerServices;
