b66b844d1cd90c52cb5d3d2ddfa56eea928fc29e
William Kent <wjk@users.noreply.github.com>
2019-09-19 20:18:33 -0400

    Remove remnant of Microsoft-internal comment

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/XmlnsPrefixAttribute.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/XmlnsPrefixAttribute.cs
@@ -2,14 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
 //  Contents: Namespace default prefix recommendation support 
-
-//  Created:   04/28/2005 Microsoft
-//
 
 using System.Runtime.CompilerServices;
 
