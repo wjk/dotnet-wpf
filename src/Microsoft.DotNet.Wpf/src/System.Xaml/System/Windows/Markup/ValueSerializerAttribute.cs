21f40ad7ee969e705c1118ea5adec460ee255c5c
William Kent <wjk@users.noreply.github.com>
2019-09-19 20:16:56 -0400

    Remove remnant of Microsoft-internal comment

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/ValueSerializerAttribute.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/ValueSerializerAttribute.cs
@@ -2,17 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
 //  Contents:  An attribute that allows associating a ValueSerializer 
 //             implementation with either a type or a property (or
 //             an attached property by setting it on the static accessor
 //             for the attachable property).
-//
-//  Created:   04/28/2005 Microsoft
-//
 
 using System;
 using System.Runtime.CompilerServices;
