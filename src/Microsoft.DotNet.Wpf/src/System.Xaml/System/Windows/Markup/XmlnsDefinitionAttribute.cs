986a003ddbae9f80a1400214bbebc50854d1cc8b
William Kent <wjk@users.noreply.github.com>
2019-09-19 20:18:09 -0400

    Remove remnant of Microsoft-internal comment

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/XmlnsDefinitionAttribute.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/XmlnsDefinitionAttribute.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
 //  Contents: Attribute that keep mapping between Xml namespace and  
 //            the known types in assembly.   
 
