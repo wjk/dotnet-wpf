4bc36740e8a08b884f0f3e3f0ad5955d765a9022
William Kent <wjk@users.noreply.github.com>
2021-01-03 21:29:21 -0500

    Have create-nupkg.bat pass its parameters
    I need this batch file to ensure the real working
    directory of PowerShell is correct. Other than that,
    PowerShell should do the argument handling.

--- eng/create-updatedwpf-nupkg.bat
+++ eng/create-updatedwpf-nupkg.bat
@@ -1,8 +1,4 @@
 @echo off
 cd %~dp0\..
 
-pwsh -ExecutionPolicy Bypass -File eng\create-updatedwpf-nupkg.ps1 ^
-  -WorkDirectory artifacts\tmp\nupkg ^
-  -SdkPackagePath artifacts\packages\Debug\NonShipping\Microsoft.NET.Sdk.WindowsDesktop.Debug.5.0.0-dev.nupkg ^
-  -BinaryPackagePath artifacts\packages\Debug\NonShipping\runtime.win-x86.Microsoft.DotNet.Wpf.GitHub.Debug.5.0.0-dev.nupkg ^
-  -NuspecTemplatePath eng\Sunburst.UpdatedWPF.nuspec
+pwsh %*
