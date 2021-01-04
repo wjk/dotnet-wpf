393dddea870a0ce760d56d0db96c3624092726c7
William Kent <wjk@users.noreply.github.com>
2021-01-04 14:19:07 -0500

    Update create-debug-nupkg.bat

--- eng/create-debug-nupkg.bat
+++ eng/create-debug-nupkg.bat
@@ -2,8 +2,9 @@
 cd %~dp0\..
 
 pwsh -ExecutionPolicy Bypass -File eng\create-updatedwpf-nupkg.ps1 ^
-  -VersionNumber 1.0.42.42 ^
+  -VersionNumber 1.0.255.255 ^
   -WorkDirectory artifacts\tmp\nupkg ^
   -SdkPackagePath artifacts\packages\Debug\NonShipping\Microsoft.NET.Sdk.WindowsDesktop.Debug.5.0.0-dev.nupkg ^
   -BinaryPackagePath artifacts\packages\Debug\NonShipping\runtime.win-x86.Microsoft.DotNet.Wpf.GitHub.Debug.5.0.0-dev.nupkg ^
+  -BinaryPackagePath64Bit artifacts\packages\Debug\NonShipping\runtime.win-x64.Microsoft.DotNet.Wpf.GitHub.Debug.5.0.0-dev.nupkg ^
   -NuspecTemplatePath eng\Sunburst.UpdatedWPF.nuspec
