1a4ae491ae635c90537b629f851e4ec45c8af665
William Kent <wjk@users.noreply.github.com>
2021-01-03 20:37:22 -0500

    Update Sunburst.UpdatedWPF script

--- eng/create-updatedwpf-nupkg.bat
+++ eng/create-updatedwpf-nupkg.bat
@@ -3,6 +3,6 @@ cd %~dp0\..
 
 pwsh -ExecutionPolicy Bypass -File eng\create-updatedwpf-nupkg.ps1 ^
   -WorkDirectory artifacts\tmp\nupkg ^
-  -SdkPackagePath artifacts\packages\Debug\NonShipping\Microsoft.NET.Sdk.WindowsDesktop.Debug.6.0.0-dev.nupkg ^
-  -BinaryPackagePath artifacts\packages\Debug\NonShipping\runtime.win-x86.Microsoft.DotNet.Wpf.GitHub.Debug.6.0.0-dev.nupkg ^
+  -SdkPackagePath artifacts\packages\Debug\NonShipping\Microsoft.NET.Sdk.WindowsDesktop.Debug.5.0.0-dev.nupkg ^
+  -BinaryPackagePath artifacts\packages\Debug\NonShipping\runtime.win-x86.Microsoft.DotNet.Wpf.GitHub.Debug.5.0.0-dev.nupkg ^
   -NuspecTemplatePath eng\Sunburst.UpdatedWPF.nuspec
